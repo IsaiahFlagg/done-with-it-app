458e9a1f82d75addede69d9bb44fb4530fc600b8
I Flagg <isaiahnflagg@gmail.com>
2020-12-02 23:36:52 -0500

    added ViewImageScreen

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import WelcomeScreen from './screens/WelcomeScreen';
+import ViewImageScreen from './screens/ViewImageScreen';
 
 export default function App() {
   return (
-    <WelcomeScreen />
+    <ViewImageScreen />
   )
 }
 
